06c70625b16df48559f78c160e0c27c6c16db4d3
mikathefinn <mikathefinn@gmail.com>
2023-10-26 13:15:02 +0300

    checkUI added, filter function added

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
-
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 const addItem = (e) => {
   e.preventDefault();
@@ -13,9 +14,7 @@ const addItem = (e) => {
     alert('You silly cunt, please add an item.');
     return;
   }
-
   // Create a list item
-
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(newItem));
   console.log(li);
@@ -28,6 +27,8 @@ const addItem = (e) => {
   console.log(li);
   itemList.appendChild(li);
   // append li to the itemList to add it to the DOM
+
+  checkUI();
   itemInput.value = '';
   //clear the input value to remove the previous entry from the form
 };
@@ -51,8 +52,53 @@ const removeItem = (e) => {
   //in html you can see that the icon has a parent with 'remove-item' class ie. the button
   if (e.target.parentElement.classList.contains('remove-item')) {
     console.log('click');
-    e.target.parentElement.parentElement.remove();
+    if (
+      confirm(
+        `Are you sure you want to remove ${e.target.parentElement.parentElement.textContent} form the list?`
+      )
+    ) {
+      e.target.parentElement.parentElement.remove();
+    }
     //target parent element's parent, the li
+  }
+  checkUI();
+};
+
+const clearItems = (e) => {
+  while (itemList.firstChild) {
+    //as long as itemList.firstChild is true, remove firstChild
+    itemList.firstChild.remove();
+  }
+  checkUI();
+};
+
+const filterItems = (e) => {
+  const items = itemList.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+  console.log(text);
+
+  items.forEach(function (item) {
+    if (!item.textContent.toLowerCase().includes(text)) {
+      item.style.display = 'none';
+    } else {
+      item.style.display = 'block';
+    }
+  });
+};
+
+const checkUI = () => {
+  const items = itemList.querySelectorAll('li');
+  // items has to be declared inside the function, otherwise it won't
+  // update the nodelist length
+  //  REMEMBER TO ADD checkUI TO FUNCTIONS THAT CHANGE THE NUMBER OF ITEMS
+
+  //remove ClearAll button and Filter field if no items displayed
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
   }
 };
 
@@ -60,3 +106,8 @@ const removeItem = (e) => {
 
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
+// check items length when loading the page
